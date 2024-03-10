cfdf11bf7e9fba44c2cdbcfa4babb5f0423b6ab9
sumit <105603147+sumitardalkar7@users.noreply.github.com>
2024-03-10 13:08:11 +0000

    datatype and var

---
+++ datatype.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newwwer veriosion
+alert(12+21)// we  are using node js not browser ... so this is not usfull here...
+console.log(23+
+    32) // code redability should be high
+
+console.log("sumit")
+let name = "mc sandesh "
+let age =12;
+//  number bigint string boolean null ->standalone value  . undifind , symbol...
+// objects 
+console.log(typeof 23);
+// null is an object 
---
+++ variables.js
@@ -0,0 +1,10 @@
+const name = "sumit"
+var nam= "sumit"
+let surname="ardalkar"
+middel="rahul"
+// single line commnt
+/* 
+multiline comment
+
+*/
+
