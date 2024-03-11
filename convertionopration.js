390dfa122378498622da05f18101f9936934f99c
sumit <105603147+sumitardalkar7@users.noreply.github.com>
2024-03-11 06:20:08 +0000

    oprations n pre fix post fix problem

---
+++ convertionopration.js
@@ -0,0 +1,4 @@
+let  scrore ="s12";
+ console.log(typeof scrore)
+ let conver=Number(scrore);
+ console.log(typeof conver)
---
+++ op.js
@@ -0,0 +1,8 @@
+let value = 23 // oprations 
+let negative=-value
+// console.log(negative)
+let greade = 21;
+console.log(++greade)
+console.log(greade++)
+console.log(greade++)
+
