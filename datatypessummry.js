673a701a6276bcd8f535c7010d0c98d7c5ee68f6
sumit <105603147+sumitardalkar7@users.noreply.github.com>
2024-03-11 07:06:37 +0000

    datatye summery

---
+++ datatypessummry.js
@@ -0,0 +1,28 @@
+// primitives 
+// call by values means  when we copy the data it will return the copy of that value 
+//  types 7 : String number boolean null undefined symbol bigInt
+// reffrance type or non premitives
+// Array object functios 
+let score = 23 
+let number = 232.1;
+let result =true;
+let outSideTemp=nuull;
+let email;
+let id = Symbol("123")
+let bigIn= 374386738637637820n;
+
+
+
+
+// refrance type of non perimitive data types 
+const friends =[ "sumit ","rahul","radhika ","ram"]
+let myobj ={
+    name : "sumit ",
+    age :12,
+    clg : dyp
+
+}
+
+ const sumit = function(){
+    console.log("hello world ")
+}
