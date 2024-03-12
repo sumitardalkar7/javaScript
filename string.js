5ed527d29bd67d33f95e256c59e3a1e9e8275cd3
sumit <105603147+sumitardalkar7@users.noreply.github.com>
2024-03-12 08:32:20 +0000

    string ke bare padhai

---
+++ string.js
@@ -0,0 +1,35 @@
+let name = "sumit"
+
+let age = 12
+// console.log( name + age +"ky ky bhava");
+// console.log(`my name is ${name} and my age is ${age} `)
+const your = new String('sumit');
+console.log(your);
+console.log(your[2]);
+console.log(your.__proto__)
+console.log(your.length)
+
+console.log(your.toLowerCase())
+console.log(your.toString)
+console.log(your.charAt(2))
+console.log(your.substring(1,3))
+console.log(your.slice(-4))
+ const newname = "    sumit   "
+ 
+
+ console.log(newname.trim())
+ let url = "hhtps://sumit.ardalllioner.com/mark$201mc "
+ console.log(url.replace("$20", '_'))
+ console.log(url.includes(`lion`))
+ console.log(newname.split("-"))
+ 
+
+
+
+
+
+
+
+
+
+// by using new keyword you also declear the string the anthor way of declearation of string
