0c7603d8ad9ea23257c2b4906401b196a9040109
sumit <105603147+sumitardalkar7@users.noreply.github.com>
2024-03-12 18:52:43 +0000

    date our time

---
+++ datelist.js
@@ -0,0 +1,8 @@
+let Mydate = new Date();
+console.log(Mydate.toString())
+console.log(Mydate.toDateString())
+console.log(Mydate.toLocaleString())
+console.log(Mydate.toTimeString())
+console.log(typeof(Mydate))
+let newDate= new Date(2023 , 11,21)
+console.log(newDate)
